710ca0a918e59488daf8d7d6d3f84f4655c5a860
Mekhala <mekhalapillutla8@gmail.com>
2024-06-22 18:57:50 +0530

    Add files via upload

---
+++ ArraysApp/src/Sample.java
@@ -0,0 +1,50 @@
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1=20;
+		int num2=30;
+		System.out.println( num1+num2 );
+		System.out.println( "Sum = "+ num1+num2 + "!");
+		
+		int i;
+		i=10;
+		System.out.println(i++);
+		System.out.println(i++);
+		System.out.println(i);
+		System.out.println(i);
+		System.out.println(++i);
+		System.out.println(i--);
+		System.out.println(i);
+		int x,y,z;
+		x=20;
+		y=1;
+		z = ++x + x++ + 10;
+		System.out.println( x ); 
+		z = ++z - --z;
+		System.out.println( z );// 1
+		System.out.println( z + ++z);//  ?
+		
+		final long tollfreeno = 180025635412L;
+		
+		tollfreeno=180012345412L;
+		if(true) {
+		}
+		if(false) {
+			System.out.println("True");
+		} //else {
+//			System.out.println("False");
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ ArraysApp/src/SumOfElements.java
@@ -0,0 +1,28 @@
+
+public class SumOfElements {
+public static void main(String[] args) {
+	float prices[] = { 563.24f,953.26f,789.25f,685.24f ,110.00f};
+	System.out.println( prices.length);
+	float sum = 0.0f;
+	for(int index = 0; index < prices.length;  sum+= prices[ index ], index++ ); {
+		//sum += prices[ index ];
+	}
+	System.out.println("The original array : ");
+	for(int index = 0;index < prices.length; index++ ) {
+		System.out.println( prices [ index ]);
+	}
+	for(float price :  prices){
+		System.out.println(price);
+	}
+	System.out.println("Sum of array elements: " + sum  );
+	
+	for(;;) {
+		System.out.println("Hi");
+	}
+	
+	
+	
+	
+	
+}
+}
